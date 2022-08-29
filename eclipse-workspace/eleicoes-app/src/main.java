eb6849dfcf413b4aafefc9054c9cfa06a657abaa
Carlos Madrid <carloseduardomadrid.2@gmail.com>
2022-08-29 00:27:32 -0300

    commit

---
+++ eclipse-workspace/eleicoes-app/src/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+	
+	public class Candidato {
+		 String nomeCandidato = "";
+		 String partidoCandidato = "";
+		 String[] fichaLimpa = {"Pode se candidatar", "Não pode se candidatar"};
+	}
+	
+	public class Pesquisa {
+		String instituto = "";
+		int data = 0;
+		String localVoto = "";
+		double mediaIdade = 0.00;
+		
+	}
+	
+	public class PesquisaCandidato  {
+		int votos = 0;
+	}
+	
+	public class TipoPesquisa{
+		String descricao = "";
+		String[] tipo = {"Induzida","Livre"};
+		String[] formato = {"Telefone", "Presencial"};
+	}
+
+}
